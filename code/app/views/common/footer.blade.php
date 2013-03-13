3ecdb8c14276d96d80cee3a4d2c3d02dbe38867f
EvilDot <github@evildotstudios.com>
2013-03-13 04:17:03 +0000

    added evildotstudios logo

--- code/app/views/common/footer.blade.php
+++ code/app/views/common/footer.blade.php
@@ -1,3 +1,7 @@
+<div class="container">
+  <a href="http://evildotstudios.com"><img src="{{URL::to('assets/img/evillogo.png')}}"></a>
+</div>    
+
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="{{URL::to('assets/js/bootstrap.min.js')}}"></script>
   </body>
